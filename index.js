054d9e98eef8182c4de9edf94c0b240384e31def
zibranhasan <zibranhasansourab@gmail.com>
2023-04-25 16:41:15 +0600

    final commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iqxuo5b.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+async function run(){
+  try{ 
+     const productCollection = client.db('emajohn').collection('products')
+
+     app.get('/products', async(req, res) => {
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
+        console.log(page, size);
+        const query = {}
+        const cursor = productCollection.find(query);
+        const products = await cursor.skip(page*size).limit(size).toArray();
+        const count = await productCollection.estimatedDocumentCount();
+        res.send({count, products});
+     });
+
+
+    app.post('/productsByIds', async( req, res) => {
+        const ids = req.body;
+        const objectIds = ids.map(id => new ObjectId(id))
+        const query = {_id: {$in: objectIds}};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+    })
+  }
+  finally{
+  }
+
+}
+run().catch( err=> console.error(err))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res)=> {
+    res.send('ema john server is running');
+})
+
+
+
+app.listen(port, () => {
+    console.log(`ema john running on:${port}`);
+})
